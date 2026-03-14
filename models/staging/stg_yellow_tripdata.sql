680764463c0be1992f8339b51725f7d2c407d6d3
Nobpanat Punkaew <nobpanat.nite@gmail.com>
2026-03-14 06:12:26 +0000

    add seeds , create intermediate model , change column in staging table , add package and add fact table .

--- models/staging/stg_yellow_tripdata.sql
+++ models/staging/stg_yellow_tripdata.sql
@@ -13,6 +13,7 @@ SELECT
     store_and_fwd_flag,
     cast(passenger_count as INT64) as passenger_count,
     cast(trip_distance as NUMERIC) as trip_distance,
+    1 as trip_type, -- yellow taxis only be street-hail (trip_type=1)
 
     -- payment info
     cast(fare_amount as NUMERIC) as fare_amount,
@@ -21,6 +22,7 @@ SELECT
     cast(tip_amount as NUMERIC) as tip_amount,
     cast(tolls_amount as NUMERIC) as tolls_amount,
     cast(improvement_surcharge as NUMERIC) as improvement_surcharge,
+    0 as ehail_fee, -- yellow taxis do not have ehail fees
     cast(total_amount as NUMERIC) as total_amount,
     cast(payment_type as INT64) as payment_type
 
